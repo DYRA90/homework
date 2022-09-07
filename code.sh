526a0c2157ced30e2a768d1c98d3a427e65a412f
DYRA90 <djiekouayossa1988@gmail.com>
2022-09-07 12:36:39 -0500

    training

---
+++ code.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# this is a code for my exercise 
+
+
+read -p "plase enter your lastname: "  last_name
+sleep 3
+read -p "please enter your fristname:" first_name
+sleep 4
+read -p " please enter your age:" Age
+sleep 7
+str_1="Hello $last_name $first_name you are $Age year old"
+sleep 8
+echo $str_1
